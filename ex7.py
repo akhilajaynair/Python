abc74ff9a65bdd30a72041b6d4eeacea2fd99ccd
akhilajaynair <40654721+akhilajaynair@users.noreply.github.com>
2018-07-24 18:08:23 +0530

    Add files via upload

---
+++ ex7.py
@@ -0,0 +1,5 @@
+print("Enter two numbers")
+a=input()
+b=input()
+c=a%b
+print('MOD='+str(c))
---
+++ ex8.py
@@ -0,0 +1,29 @@
+print("Enter the number")
+
+x=input()
+
+def fibonacci(x):
+
+  s=[]
+
+  i=0
+
+  x=0
+
+  y=1
+
+  s.append(a)
+
+  s.append(b)
+
+  while i<a:
+
+    (a,b)=(b,a+b)
+
+    s.append(b)
+
+    i=i+1 
+
+  print(s)
+
+fibonacci(a)
